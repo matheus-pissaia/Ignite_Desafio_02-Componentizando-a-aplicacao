0df5c0bd64ea76dc21eecefe3dc70dc371114f11
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-05-27 00:15:58 -0300

    🎉 Desafio Finalizado!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+
+import { MovieProps } from '../App';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: {
+    title: string;
+  }
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <Header selectedGenre={props.selectedGenre} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
